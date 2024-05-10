074632bbceeae3ae2f557bccce02553d9715ab55
Suraj Sharma <125715507+surajsharma111@users.noreply.github.com>
2024-05-10 13:59:56 +1000

    setup backend

---
+++ Backend/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require('cors')
+const app = express();
+
+require('dotenv').config();
+const PORT = process.env.PORT
+
+// Middlewares
+
+app.use(express.json())
+app.use(cors())
+const server = () =>{
+    app.listen(PORT, () => {
+        console.log('listening to port : ', PORT)
+    })
+}
+server()
